799920291e49b08990f832e66507396a3c2c04d5
Alex Kalaitzidis <alexkalaitzidis2@gmail.com>
2021-01-06 12:21:03 +0200

    Create local eth API

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 
 const emojis = require('./emojis');
+const eth = require('./eth');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({
-    message: 'API - 👋🌎🌍🌏'
-  });
+	res.json({
+		message: 'API - 👋🌎🌍🌏',
+	});
 });
 
 router.use('/emojis', emojis);
+router.use('/eth', eth);
 
 module.exports = router;
