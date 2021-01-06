fedcd54bc201154a655a7836d816018c7fcc305f
Alex Kalaitzidis <alexkalaitzidis2@gmail.com>
2021-01-06 12:20:48 +0200

    Indent with tabs

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-    env: {
-        jest: true,
-    },
-    extends: 'airbnb-base',
-    rules: {
-        'comma-dangle': 0,
-        'no-underscore-dangle': 0,
-        'no-param-reassign': 0,
-        'no-return-assign': 0,
-        camelcase: 0,
-    }
+	env: {
+		jest: true,
+	},
+	extends: 'airbnb-base',
+	rules: {
+		indent: [4, 'tab'],
+		'comma-dangle': 0,
+		'no-underscore-dangle': 0,
+		'no-param-reassign': 0,
+		'no-return-assign': 0,
+		camelcase: 0,
+	}
 };
