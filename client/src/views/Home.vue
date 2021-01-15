fd5f0fec9f1ddc78d2024d19e9cec77616b62bd9
Alex Kalaitzidis <alexkalaitzidis2@gmail.com>
2021-01-15 22:48:42 +0200

    Add home search

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,37 @@
 <template>
-  <v-container>
-    <v-row align="center" justify="center">
-      <v-col cols="12">
-        Welcome to ethereum contract scanner
-      </v-col>
-    </v-row>
-  </v-container>
+	<v-container>
+		<v-row align="center" justify="center">
+			<v-col cols="10">
+				<h1 class="text-center">
+					Welcome to ethereum contract scanner
+				</h1>
+				<v-text-field
+					outlined
+					v-model="search"
+					append-icon="mdi-magnify"
+					label="Search a block hash"
+					single-line
+					hide-details
+					@keyup.enter="$router.push(`/block/` + search)"
+				></v-text-field>
+				<div class="pa-2 text-center">
+					<v-btn rounded color="primary" @click="$router.push(`/block/` + search)"
+						>Search block</v-btn
+					>
+				</div>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
 export default {
-	name: 'Home',
+	data() {
+		return {
+			search: ''
+		};
+	},
+
+	name: 'Home'
 };
 </script>
