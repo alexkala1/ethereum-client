b2c06fc588ae95559aa4198c9e44decbbd18c4be
Alex Kalaitzidis <alexkalaitzidis2@gmail.com>
2021-01-15 22:47:57 +0200

    Rename properly views

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import BlockResults from '../views/BlockResults.vue';
 
 Vue.use(VueRouter);
 
@@ -8,22 +9,19 @@ const routes = [
 	{
 		path: '/',
 		name: 'Home',
-		component: Home,
+		component: Home
 	},
 	{
-		path: '/about',
-		name: 'About',
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-	},
+		path: '/block/:id',
+		name: 'Block Results',
+		component: BlockResults
+	}
 ];
 
 const router = new VueRouter({
 	mode: 'history',
 	base: process.env.BASE_URL,
-	routes,
+	routes
 });
 
 export default router;
