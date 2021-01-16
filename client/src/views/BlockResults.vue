201f76f7eac3d4ee97dc2f58d2588b7d74ffd0f3
Alex Kalaitzidis <alexkalaitzidis2@gmail.com>
2021-01-16 10:58:15 +0200

    Refactor table and link to transaction in expansion details

--- client/src/views/BlockResults.vue
+++ client/src/views/BlockResults.vue
@@ -15,7 +15,11 @@
 						<v-spacer></v-spacer>
 						<v-switch
 							v-model="contractSwitch"
-							:label="contractSwitch ? 'Show all transactions' : 'Show only contracts'"
+							:label="
+								contractSwitch
+									? 'Show all transactions'
+									: 'Show only contracts'
+							"
 							@click="showOnlyContracts()"
 						></v-switch>
 					</v-card-title>
@@ -25,12 +29,10 @@
 						show-expand
 						single-expand
 						item-key="hash"
-						@click:row="
-							$router.push('/transaction/' + $route.params.id)
-						"
 						:headers="headers"
 						:items="blockResults"
 						:search="search"
+						sort-by="transactionIndex"
 					>
 						<template v-slot:expanded-item="{ headers, item }">
 							<td :colspan="headers.length">
@@ -46,6 +48,32 @@
 												</div>
 											</v-list-item-content>
 										</v-list-item>
+									</v-col>
+								</v-row>
+								<v-row>
+									<v-col cols="6">
+										<v-tooltip top>
+											<template
+												v-slot:activator="{ on, attrs }"
+											>
+												<v-list-item
+													three-line
+													v-bind="attrs"
+													v-on="on"
+													:to="/transaction/ + item.hash"
+												>
+													<v-list-item-content>
+														<v-list-item-title>
+															Hash:
+														</v-list-item-title>
+														<div class="contract">
+															{{ item.hash }}
+														</div>
+													</v-list-item-content>
+												</v-list-item>
+											</template>
+											<span>Go to transaction</span>
+										</v-tooltip>
 									</v-col>
 								</v-row>
 								<v-row>
@@ -137,20 +165,21 @@ export default {
 			allTransactions: [],
 			search: '',
 			headers: [
+				{ text: 'transactionIndex', value: 'transactionIndex' },
 				{ text: 'blockHash', value: 'blockHash' },
 				// { text: 'blockNumber', value: 'blockNumber' },
 				{ text: 'from', value: 'from' },
 				{ text: 'to', value: 'to' },
+				{ text: 'value', value: 'value' },
+
 				{ text: 'gas', value: 'gas' },
-				{ text: 'gasPrice', value: 'gasPrice' },
-				{ text: 'hash', value: 'hash' },
+				{ text: 'gasPrice', value: 'gasPrice' }
+				// { text: 'hash', value: 'hash' },
 				// { text: 'input', value: 'input' },
 				// { text: 'nonce', value: 'nonce' },
 				// { text: '1st 32 bytes', value: 'r' },
 				// { text: 'Next 32 bytes', value: 's' },
-				{ text: 'transactionIndex', value: 'transactionIndex' },
 				// { text: 'Recovery Value', value: 'v' },
-				{ text: 'value', value: 'value' }
 			],
 			snackbar: false,
 			snackbarError: '',
